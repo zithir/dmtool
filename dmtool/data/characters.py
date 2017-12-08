f1d9c53804014ff693309573e017ed0181974d7c
krevi <krevi@DESKTOP-EPVCNKI.local>
2017-12-08 18:17:13 +0100

    Abbreviation corrections

--- dmtool/data/characters.py
+++ dmtool/data/characters.py
@@ -14,23 +14,23 @@ ability_best = {
 # Manually created list of best order of abilities for a given class
 # The order can be adjusted
 # Descending order (from high priority to low)
-                'fighter':['Str', 'Con', 'Cha', 'Dex', 'Wis', 'Int'],
-                'rogue'  :['Dex', 'Int', 'Wis', 'Str', 'Con', 'Cha'],
-                'wizard'   :['Int', 'Wis', 'Char', 'Con', 'Dex', 'Str'],
-                'barbarian' :['Str', 'Dex', 'Con', 'Chr', 'Wis', 'Int'],
-                'paladin' :['Con', 'Str', 'Wis', 'Chr', 'Int', 'Dex'],
-                'sorcerer' :['Chr', 'Int', 'Con', 'Dex', 'Str', 'Wis'],
-                'bard' :['Chr', 'Dex', 'Int', 'Con', 'Str', 'Wis'],
-                'cleric' :['Wis', 'Con', 'Chr', 'Int', 'Str', 'Dex'],
-                'druid' :['Wis', 'Int', 'Dex', 'Chr', 'Con', 'Str'],
-                'monk' :['Wis', 'Dex', 'Str', 'Con', 'Int', 'Chr'],
-                'ranger' :['Dex', 'Str', 'Wis', 'Con', 'Int', 'Chr'],
+                'fighter'   :['Str', 'Con', 'Cha', 'Dex', 'Wis', 'Int'],
+                'rogue'     :['Dex', 'Int', 'Wis', 'Str', 'Con', 'Cha'],
+                'wizard'    :['Int', 'Wis', 'Cha', 'Con', 'Dex', 'Str'],
+                'barbarian' :['Str', 'Dex', 'Con', 'Cha', 'Wis', 'Int'],
+                'paladin'   :['Con', 'Str', 'Wis', 'Cha', 'Int', 'Dex'],
+                'sorcerer'  :['Cha', 'Int', 'Con', 'Dex', 'Str', 'Wis'],
+                'bard'      :['Cha', 'Dex', 'Int', 'Con', 'Str', 'Wis'],
+                'cleric'    :['Wis', 'Con', 'Cha', 'Int', 'Str', 'Dex'],
+                'druid'     :['Wis', 'Int', 'Dex', 'Cha', 'Con', 'Str'],
+                'monk'      :['Wis', 'Dex', 'Str', 'Con', 'Int', 'Cha'],
+                'ranger'    :['Dex', 'Str', 'Wis', 'Con', 'Int', 'Cha'],
                 
                 }
 
 ability_adjustment = {
-                    'dwarf': {'Con': 2, 'Cha':-2},
-                    'elf': {'Dex': 2, 'Con':-2},
-                    'gnome':{'Con': 2, 'Str':-2},
+                    'dwarf'   : {'Con': 2, 'Cha':-2},
+                    'elf'     : {'Dex': 2, 'Con':-2},
+                    'gnome'   : {'Con': 2, 'Str':-2},
                     'half-orc': {'Str': 2, 'Int':-2, 'Chr':-2},
                     }
