05c139e560d16a300a526c521ac05851911f5dc2
krevi <krevi@DESKTOP-EPVCNKI.local>
2017-12-08 12:01:57 +0100

    Races and gitignore
    completed vanilla race list and atribute mods; added gitignore for
    Eclipse

--- dmtool/data/characters.py
+++ dmtool/data/characters.py
@@ -4,8 +4,8 @@ into xmls.
 """
 
 
-races = ["human", "elf", "dwarf"]
-classes = ["fighter", "rogue", "wizard"]
+races = ["human", "elf", "dwarf", "gnome", "half-elf", "half-orc", "halfling"]
+classes = ["fighter", "rogue", "wizard", "barbarian", "paladin", "sorcerer", "bard", "cleric", "druid", "monk", "ranger"]
 
 # Fixed ability order that should be used any time
 ability_names = ['Str', 'Dex', 'Con', 'Int', 'Wis', 'Cha']
@@ -16,10 +16,21 @@ ability_best = {
 # Descending order (from high priority to low)
                 'fighter':['Str', 'Con', 'Cha', 'Dex', 'Wis', 'Int'],
                 'rogue'  :['Dex','Int','Wis', 'Con', 'Str', 'Cha'],
-                'wizard'   :['Int', 'Dex', 'Con', 'Wis', 'Cha', 'Str']
+                'wizard'   :['Int', 'Dex', 'Con', 'Wis', 'Cha', 'Str'],
+                'barbarian' :['Str', 'Dex', 'Con', 'Wis', 'Chr', 'Int'],
+                'paladin' :['Chr', 'Str', 'Wis', 'Con', 'Int', 'Dex'],
+                'sorcerer' :['Chr', ''],
+                'bard' :[],
+                'cleric' :[],
+                'druid' :[],
+                'monk' :[],
+                'ranger' :[],
+                
                 }
 
 ability_adjustment = {
                     'dwarf': {'Con': 2, 'Cha': -2},
-                    'elf': {'Dex': 2, 'Con': -2}
+                    'elf': {'Dex': 2, 'Con': -2},
+                    'gnome':{'Con': 2, 'Str': -2},
+                    'half-orc': {'Str': 2, 'Int': -2, 'Chr': -2},
                     }
