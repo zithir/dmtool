f809017590b3f131b8e82ae91048efb8c58cca61
krevi <krevi@DESKTOP-EPVCNKI.local>
2017-12-08 15:08:24 +0100

    Finished ability_best lib

--- dmtool/data/characters.py
+++ dmtool/data/characters.py
@@ -3,9 +3,9 @@ Data regarding player characters. In the future they should be converted
 into xmls.
 """
 
-
 races = ["human", "elf", "dwarf", "gnome", "half-elf", "half-orc", "halfling"]
-classes = ["fighter", "rogue", "wizard", "barbarian", "paladin", "sorcerer", "bard", "cleric", "druid", "monk", "ranger"]
+classes = ["fighter", "rogue", "wizard", "barbarian", "paladin", "sorcerer",
+           "bard", "cleric", "druid", "monk", "ranger"]
 
 # Fixed ability order that should be used any time
 ability_names = ['Str', 'Dex', 'Con', 'Int', 'Wis', 'Cha']
@@ -15,22 +15,22 @@ ability_best = {
 # The order can be adjusted
 # Descending order (from high priority to low)
                 'fighter':['Str', 'Con', 'Cha', 'Dex', 'Wis', 'Int'],
-                'rogue'  :['Dex','Int','Wis', 'Con', 'Str', 'Cha'],
-                'wizard'   :['Int', 'Dex', 'Con', 'Wis', 'Cha', 'Str'],
-                'barbarian' :['Str', 'Dex', 'Con', 'Wis', 'Chr', 'Int'],
-                'paladin' :['Chr', 'Str', 'Wis', 'Con', 'Int', 'Dex'],
-                'sorcerer' :['Chr', ''],
-                'bard' :[],
-                'cleric' :[],
-                'druid' :[],
-                'monk' :[],
-                'ranger' :[],
+                'rogue'  :['Dex', 'Int', 'Wis', 'Str', 'Con', 'Cha'],
+                'wizard'   :['Int', 'Wis', 'Char', 'Con', 'Dex', 'Str'],
+                'barbarian' :['Str', 'Dex', 'Con', 'Chr', 'Wis', 'Int'],
+                'paladin' :['Con', 'Str', 'Wis', 'Chr', 'Int', 'Dex'],
+                'sorcerer' :['Chr', 'Int', 'Con', 'Dex', 'Str', 'Wis'],
+                'bard' :['Chr', 'Dex', 'Int', 'Con', 'Str', 'Wis'],
+                'cleric' :['Wis', 'Con', 'Chr', 'Int', 'Str', 'Dex'],
+                'druid' :['Wis', 'Int', 'Dex', 'Chr', 'Con', 'Str'],
+                'monk' :['Wis', 'Dex', 'Str', 'Con', 'Int', 'Chr'],
+                'ranger' :['Dex', 'Str', 'Wis', 'Con', 'Int', 'Chr'],
                 
                 }
 
 ability_adjustment = {
-                    'dwarf': {'Con': 2, 'Cha': -2},
-                    'elf': {'Dex': 2, 'Con': -2},
-                    'gnome':{'Con': 2, 'Str': -2},
-                    'half-orc': {'Str': 2, 'Int': -2, 'Chr': -2},
+                    'dwarf': {'Con': 2, 'Cha':-2},
+                    'elf': {'Dex': 2, 'Con':-2},
+                    'gnome':{'Con': 2, 'Str':-2},
+                    'half-orc': {'Str': 2, 'Int':-2, 'Chr':-2},
                     }
